f116cee7c8a357d494db0c21ce739ebb496cc7d8
MubeenAnjum <mubeenanjum264@gmail.com>
2025-03-08 22:17:07 +0400

    add utils code for customer api error and response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotevn from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotevn.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failded!!!", error);
+  });
