fbaae34b7e1e1425c158784e1b393527f4a3d4c3
MubeenAnjum <mubeenanjum264@gmail.com>
2025-03-10 20:39:11 +0400

    add the middleware for multer for file uploading and add the cloudinary configuration and also add the our first controller and route for user register

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16KB" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//declare the routes
+app.use("/api/v1/users", userRouter);
+
 export { app };
